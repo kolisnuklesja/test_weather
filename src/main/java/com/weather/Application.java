2b69704b8bc0463fd823d8985a3bd0e937e3e6dc
Ta1-1echka <polovinkina2401@gmail.com>
2017-01-24 16:30:31 +0300

    *

---
+++ src/main/java/com/weather/Application.java
@@ -0,0 +1,25 @@
+package com.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+
+/**
+ * Created by Tanya on 24.01.2017.
+ */
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
--- src/main/java/com/weather/WeatherController.java
+++ src/main/java/com/weather/WeatherController.java
@@ -1,19 +1,21 @@
-package controller;
+package com.weather;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Tanya on 24.01.2017.
  */
+
 @Controller
 public class WeatherController {
 
     @RequestMapping("/")
-    public ModelAndView getIndexPage() {
+    public ModelAndView welcome() {
         return new ModelAndView("index");
     }
+
 
 }
