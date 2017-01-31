069b59d835fa863acb0cd60166e8a71e2edd5fad
Ta1-1echka <polovinkina2401@gmail.com>
2017-01-31 14:37:03 +0300

    test

---
+++ src/test/java/com/weather/config/WebSecurityConfigTest.java
@@ -0,0 +1,65 @@
+package com.weather.config;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by Tanya on 30.01.2017.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {WebConfig.class, WebSecurityConfig.class})
+@WebAppConfiguration
+public class WebSecurityConfigTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+
+    @Test
+    @WithAnonymousUser
+    public void configure() throws Exception {
+        mvc.perform(get("/login")).andExpect(forwardedUrl("/error403"));
+    }
+
+    @Test
+    public void configure2() throws Exception {
+        mvc.perform(get("/login")
+                .with(user("test").password("test").roles("ANONYMOUS")))
+                .andExpect(forwardedUrl("/error403"));
+    }
+
+
+    @Test
+    @PreAuthorize("authenticated")
+    public void configure4() throws Exception {
+        mvc.perform(get("/login")).andExpect(status().is3xxRedirection()).andDo(print());
+    }
+
+}
