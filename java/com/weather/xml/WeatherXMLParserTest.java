30cff5078b61739a5f16780c87d11e4b351aba04
Ta1-1echka <polovinkina2401@gmail.com>
2017-01-31 09:11:05 +0300

    *

--- java/com/weather/xml/WeatherXMLParserTest.java
+++ java/com/weather/xml/WeatherXMLParserTest.java
@@ -1,7 +1,6 @@
 package com.weather.xml;
 
 import com.weather.entity.*;
-import com.weather.xml.WeatherXMLParser;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
